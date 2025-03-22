9f7ec1a58bf8774e39978f7b9645ca9fd0c487ff
ltduy6 <lamthanhduy2k4@gmail.com>
2025-03-22 23:41:28 +0700

    add config for database connection

---
+++ config/config.py
@@ -0,0 +1,17 @@
+from dotevn import load_dotenv
+import os
+
+load_dotenv()
+host = os.getenv("HOST")
+port = os.getenv("PORT")
+database = os.getenv("DATABASE")
+user = os.getenv("USER")
+password = os.getenv("PASSWORD")
+
+db_params = {
+    "host": host,
+    "port": port,
+    "database": database,
+    "user": user,
+    "password": password
+}
