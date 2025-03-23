696a2fd84769b3d7da7375591a689c3547be7f5e
ltduy6 <lamthanhduy2k4@gmail.com>
2025-03-24 00:46:36 +0700

    create trading_data folder

---
+++ create_folders.py
@@ -0,0 +1,5 @@
+from src.data.service import DataService
+
+if __name__ == "__main__":
+    data_service = DataService()
+    data_service.create_data_folder()
--- src/data/service.py
+++ src/data/service.py
@@ -2,6 +2,7 @@ import psycopg2
 from typing import List
 from datetime import datetime, time
 import pandas as pd
+import os
 
 from src.data.query import *
 from config.config import *
@@ -10,7 +11,16 @@ class DataService:
     def __init__(self):
         self.connection = psycopg2.connect(**db_params)
 
-    def execute_query(self, query, from_date, to_date):
+    def create_data_folder(self):
+        if not os.path.exists("trading_data"):
+            os.makedirs("trading_data")
+
+        for i in range(2021, 2025):
+            tick = self.__fetch_tick_price(i)
+
+            tick.to_csv(f"trading_data/{i}_tick.csv", index=False)
+
+    def __execute_query(self, query, from_date, to_date):
         cursor = self.connection.cursor()
         try:
             cursor.execute(query, (from_date, to_date))
@@ -23,24 +33,28 @@ class DataService:
             print(f"Error: {e}")
 
 
-    def get_tick_price(self, year):
+    def __fetch_tick_price(self, year):
         from_date = f"{year}-01-01"
         to_date = f"{year}-12-31"
 
         columns = ["Datetime", "Price", "Volume"]
-        matched = pd.DataFrame(self.execute_query(matched_tick_query, from_date, to_date), columns=columns)
+        matched = pd.DataFrame(self.__execute_query(matched_tick_query, from_date, to_date), columns=columns)
         matched = matched.astype({"Price": float, "Volume": int})
 
         return matched
 
+    def get_tick_price(self, year):
+        tick = pd.read_csv(f"trading_data/{year}_tick.csv")
+        tick['Datetime'] = pd.to_datetime(tick['Datetime'])
+        return tick
 
-    def get_daily(self, year):
-        from_date = f"{year}-01-01"
-        to_date = f"{year}-12-31"
+    # def get_daily(self, year):
+    #     from_date = f"{year}-01-01"
+    #     to_date = f"{year}-12-31"
 
-        columns = ["Datetime", "Open", "Close", "High", "Low"]
-        daily = pd.DataFrame(self.execute_query(daily_query, from_date, to_date), columns=columns)
-        daily = daily.astype({"Open": float, "Close": float, "High": float, "Low": float})
+    #     columns = ["Datetime", "Open", "Close", "High", "Low"]
+    #     daily = pd.DataFrame(self.execute_query(daily_query, from_date, to_date), columns=columns)
+    #     daily = daily.astype({"Open": float, "Close": float, "High": float, "Low": float})
 
-        return daily
+    #     return daily
 
