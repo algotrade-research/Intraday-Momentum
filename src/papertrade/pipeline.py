c1caa51138dfda316bfe20adebcbc3b8439a152f
ltduy6 <lamthanhduy2k4@gmail.com>
2025-03-28 09:16:09 +0700

    update papertrade

--- src/papertrade/pipeline.py
+++ src/papertrade/pipeline.py
@@ -11,14 +11,13 @@ from datetime import datetime
 from config.config import redis_host, redis_port, redis_password
 from src.strategy.ORB import Intraday_ORB_strategy
 from util.date_management import *
-from metrics.metric import Metric
+from src.metrics.metric import Metric
 
 class Pipeline():
     def __init__(self):
         self.logger = None
         self.redis_client = redis.Redis(host=redis_host, port=redis_port, password=redis_password)
         self.redis_client.ping()
-        self.logger = self._init_logging()
         self.cur_date = None
         self.time_zone = pytz.timezone('Asia/Ho_Chi_Minh')
         self.strategy = None
@@ -27,10 +26,10 @@ class Pipeline():
         self.return_dates = []
         self.folder_path = 'papertrade_results'
         self.ORB_path = os.path.join(self.folder_path, 'ORB_strategy')
-
+        self.finish_trade = False
 
     
-    def _init_logging(self, log_file = 'log.txt', name='logger'):
+    def _init_logging(self, log_file = 'log.log', name='logger'):
         """
         Initialize logger
         """
@@ -40,16 +39,11 @@ class Pipeline():
         file_handler = logging.FileHandler(log_file)
         file_handler.setLevel(logging.DEBUG)
 
-        stream_handler = logging.StreamHandler()
-        stream_handler.setLevel(logging.DEBUG)
-
         formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
         file_handler.setFormatter(formatter)
-        stream_handler.setFormatter(formatter)
 
         logger.handlers.clear()
         logger.addHandler(file_handler)
-        logger.addHandler(stream_handler)
 
         return logger
     
@@ -61,16 +55,37 @@ class Pipeline():
         self.logger.info(f"Profit: {metric.final_pnl()}")
         self.logger.info(f"Maximum drawdown: {metric.maximum_drawdown()}")
     
-    def plot_results(self, directory):
+    def _plot_results(self, directory):
         """
         Plot the assets by adding the returns and save the plot
         """
-        plt.plot(self.return_dates, np.cumsum(self.returns) + self.initial_asset)
+        plt.plot(self.return_dates, np.cumsum(self.returns))
         plt.xlabel('Date')
-        plt.ylabel('Asset')
-        plt.title('Asset over time with ORB strategy')
+        plt.ylabel('Return')
+        plt.title('Return over time with ORB strategy')
         filename = os.path.join(directory, 'asset.png')
         plt.savefig(filename)
+
+    def _create_directory(self):
+        if not os.path.exists(self.folder_path):
+            os.makedirs(self.folder_path)
+        if not os.path.exists(self.ORB_path):
+            os.makedirs(self.ORB_path)
+
+    def _export_results(self, directory):
+        """
+        Export the results to a csv file
+        """
+        if not os.path.exists(directory):
+            os.makedirs(directory)
+        
+        df = pd.DataFrame({
+            'Date': self.return_dates,
+            'Return': self.returns
+        })
+        filename = os.path.join(directory, 'results.csv')
+        df.to_csv(filename, index=False)
+        self.logger.info(f"Results exported to {filename}")
 
     def ORB_papertrade(self):
         """
@@ -88,13 +103,17 @@ class Pipeline():
             if cur_price is None:
                 return
             
+            self.logger.info(f'{now} - Price: {cur_price} - Volume: {cur_volume}')
+            
             if self.cur_date is None or self.cur_date != now.date():
                 self.cur_date = now.date()
                 self.strategy = Intraday_ORB_strategy(period=30, stop_loss=2, take_profit=2)
+                self.finish_trade = False
             
             self.strategy.get_tick(now, cur_price)
 
-            if self.strategy.get_return() is not None:
+            if self.strategy.get_return() is not None and not self.finish_trade:
+                self.finish_trade = True
                 self.logger.info(f'{now} - Return: {self.strategy.get_return()}')
                 self.returns.append(self.strategy.get_return())
                 self.return_dates.append(now.date())
@@ -103,10 +122,12 @@ class Pipeline():
                 self.plot_results(self.ORB_path)
                 return
 
-    
-        current_date = datetime.now().astimezone(self.time_zone)
-        ticker_symbol = make_date_to_tickersymbol(current_date)
+        self._create_directory()
+        self.logger = self._init_logging(log_file=os.path.join(self.ORB_path, 'log.log'))
+        self.logger.info("Starting papertrade")
+        ticker_symbol = get_current_tickersymbol()
         F1M_CHANNEL = f'HNXDS:{ticker_symbol}'
+        print(f"Listening to channel {F1M_CHANNEL}")
         pub_sub = self.redis_client.pubsub()
         pub_sub.psubscribe(**{F1M_CHANNEL: message_handler})    
         pubsub_thread = pub_sub.run_in_thread(sleep_time=1)
--- util/date_management.py
+++ util/date_management.py
@@ -1,54 +1,17 @@
 from datetime import datetime, timedelta
 
-def make_date_to_tickersymbol(date_obj):
-    if type(date_obj) is str:
-        date_obj = make_date_from_string(date_obj)
-
-    maturity_date = get_maturity_date(date_obj)
-    
-    year = date_obj.strftime("%y")
-    month = date_obj.strftime("%m")
-
-    if date_obj > maturity_date:
-        month = str(int(month) + 1)
-        # format month is two digit, examples 01, 02, 03 ,04 ,05
-        month = month.zfill(2)
-        if month == "13":
-            month = "01"
-            year = str(int(year) + 1)
-
-    return f"VN30F{year}{month}"
-
-def make_date_from_string(input_date):
-    if type(input_date) is str:
-        try:
-            if "," in input_date:
-                input_date = input_date.replace(",", ".")
-            if "." in input_date:
-                date_obj = datetime.strptime(input_date, "%Y-%m-%d %H:%M:%S.%f")
-            elif ":" in input_date:
-                date_obj = datetime.strptime(input_date, "%Y-%m-%d %H:%M:%S")
-            else:
-                date_obj = datetime.strptime(input_date, "%Y-%m-%d")
-        except ValueError:
-            raise ValueError("Invalid date format. Please provide a date in the format '%Y-%m-%d' or '%Y-%m-%d %H:%M:%S.%f'. ")
+def get_current_tickersymbol():
+    year = datetime.now().year
+    month = datetime.now().month
+    day = datetime.now().day
+    if day > 15:
+        month += 1
+        if month > 12:
+            month = 1
+            year += 1
+    if month < 10:
+        month = f"0{month}"
     else:
-        date_obj = input_date
-    return date_obj
-
-def get_maturity_date(date):
-    """Get the third thursday of the month of the given date
-    Args:
-        date (datetime): _description_
-
-    Returns:
-        datetime: _description_
-    """
-    if type(date) is str:
-        date = make_date_from_string(date)
-    month = date.strftime("%m")
-    year = date.strftime("%y")
-    first_day_of_month = datetime.strptime(f"20{year}-{month}-01", "%Y-%m-%d")
-    first_thursday = first_day_of_month + timedelta(days=((3-first_day_of_month.weekday()) % 7))
-    third_thursday = first_thursday + timedelta(days=14)
-    return third_thursday
+        month = str(month)
+    year = str(year)[-2:]
+    return f"VN30F{year}{month}"
